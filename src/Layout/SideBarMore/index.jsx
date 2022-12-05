ad2658cadb306e4be1363da09298833572ef6427
lepeshka 25 <ruslanmuratbekov1972@gmail.com>
2022-12-05 18:35:19 +0600

    finish layout

--- src/Layout/SideBarMore/index.jsx
+++ src/Layout/SideBarMore/index.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
 import cn from "classnames";
 import follow from '../../assets/Follow.svg';
+import axios from "axios";
+import {useParams} from "react-router-dom";
 import cs from './style.module.scss';
 
 const SideBarMore = ({className , ...props}) => {
+	const {names} = useParams()
+	const [user , setUser] = React.useState(null)
+
+	React.useEffect(() => {
+		axios.get(`https://api.github.com/users/${names}`)
+			.then(res => setUser(res.data))
+	}, [names])
+
+	const {value} = React.useMemo(() => ({
+		value: user
+	}), [user])
+
+	if(!value){
+		return <div></div>
+	}
+
 	return (
 		<div {...props} className={cn(cs.sideBarMore , className)}>
 
 			<div className={cs.user_profile}>
 				<img
 					className={cs.avatar}
-					src="https://avatars.githubusercontent.com/u/104206949?v=4"
+					src={value.avatar_url}
 					alt=""
 				/>
 				<div className={cs.container_name}>
-					<h1>lepeshka25</h1>
-					<p>Ruslan</p>
+					<h1>{value.login}</h1>
+					<p>{value.name}</p>
 				</div>
 			</div>
 
@@ -24,13 +42,13 @@ const SideBarMore = ({className , ...props}) => {
 				<div className={cs.container_bio_followers}>
 					<div className={cs.container_bio}>
 						<p>
-							Never lose yourself
+							{value.bio}
 						</p>
 					</div>
 					<div className={cs.container_followers}>
 						<img className={cs.icon} src={follow} alt=""/>
-						<p><span>2</span>followers</p>
-						<p><span>7</span>following</p>
+						<p><span>{value.followers}</span>followers</p>
+						<p><span>{value.following}</span>following</p>
 					</div>
 				</div>
 
--- src/Pages/More/index.jsx
+++ src/Pages/More/index.jsx
@@ -1,14 +1,70 @@
 import React from 'react';
 import {WithLayoutMore} from "../../Layout/Layout";
 import Navigate from "./Navigate";
+import StarDropDown from "../../components/StarDropDown";
+import axios from "axios";
+import {useParams} from "react-router-dom";
 import cs from './More.module.scss';
 
 const More = () => {
+	const {names} = useParams()
+	const [data , setData] = React.useState([])
+
+	React.useEffect(() => {
+		axios.get(`https://api.github.com/users/${names}/repos`)
+			.then(res => setData(res.data))
+	}, [names])
+
+	const value = React.useMemo(() => ({
+		data
+	}), [data])
+
 	return (
 		<div className={cs.more}>
 			<Navigate/>
+
+			<div className={cs.container_cards}>
+
+				{
+					value?.data.map(({name, language , updated_at, visibility, id}) => (
+						<div key={id} className={cs.card}>
+
+							<div className={cs.info_repository}>
+								<p className={cs.name_repository}>{name}</p>
+								<span className={cs.public}>{visibility}</span>
+							</div>
+
+							<div className={cs.btn_container}>
+								<StarDropDown/>
+							</div>
+
+							<div className={cs.update_lang_repository}>
+								<p
+									className={cs.lang}>
+									<span
+										style={language
+											? {background: `var(--${language})`}
+											: {background: 'yellow'}
+										}
+									>
+									</span>
+									{language}
+								</p>
+								<span className={cs.update}>{updated_at}</span>
+							</div>
+
+						</div>
+					))
+				}
+
+			</div>
+
 		</div>
 	);
 };
 
 export default WithLayoutMore(More);
+
+//name
+//language
+//updated_at
